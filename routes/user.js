c6be37446687e80ad9d3d17924317f5890966d88
Trung Tran <tqtran189@gmail.com>
2020-02-23 00:44:37 -0500

    Edit views

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.send('Login');
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.send('Register');
 });
 
