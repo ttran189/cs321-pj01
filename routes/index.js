e11a9e62926493daeef6d34070eb6931634e3103
Trung Tran <tqtran189@gmail.com>
2020-02-23 03:30:40 -0500

    Views modified

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,23 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Home'
   });
 });
+
+/* GET login page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    title: 'Login'
+  });
+});
+
+/* GET register page. */
+router.get('/register', function (req, res, next) {
+  res.render('register', {
+    title: 'Register'
+  });
+});
+
 
 module.exports = router;
