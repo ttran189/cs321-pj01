525afbb5764aeb6bf34e3aabc023adc9ee8fa559
Trung Tran <tqtran189@gmail.com>
2020-02-24 02:41:47 -0500

    Routed Index to Login

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const {
-  ensureAuthenticated
+	ensureAuthenticated
 } = require('../config/auth');
 
 /* GET home page. */
+// router.get('/', function (req, res, next) {
+// 	res.render('index', {
+// 		title: 'Home'
+// 	});
+// });
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Home'
-  });
+	res.redirect('/login');
 });
 
 /* GET dashboard page. */
 router.get('/dashboard', ensureAuthenticated, (req, res, next) =>
-  res.render('dashboard', {
-    title: 'Dashboard',
-    name: req.user.name
-  }));
+	res.render('dashboard', {
+		title: 'Dashboard',
+		name: req.user.name
+	}));
 
 module.exports = router;
